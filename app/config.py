e8c381a0ecf059af5f4ea6440a46edb593af9879
N00budrag0n <helmiagung2468@gmail.com>
2025-11-27 10:06:13 +0700

    config > update variables

--- app/config.py
+++ app/config.py
@@ -1,16 +1,20 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/postgres"
-    MQTT_BROKER_HOST: str = "your-cloudamqp-host"
-    MQTT_BROKER_PORT: int = 1883
-    MQTT_USERNAME: str = ""
-    MQTT_PASSWORD: str = ""
-    MQTT_TOPIC: str = "sensors/#"
-    ML_MODEL_PATH: str = "app/ml/model.pkl"
-    DOWNLOAD_FOLDER: str = "/shared/downloads"
+    DATABASE_URL: str 
+    MQTT_BROKER_HOST: str 
+    MQTT_BROKER_PORT: int 
+    MQTT_USERNAME: str 
+    MQTT_PASSWORD: str 
+    MQTT_TOPIC: str 
+    ML_MODEL_PATH: str 
+    DOWNLOAD_FOLDER: str 
 
+    VENDOR_URL: str 
+    USERNAME: str 
+    PASSWORD: str 
+    DOWNLOAD_FOLDER: str 
 
     class Config:
         env_file = ".env"
