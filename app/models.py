dbb5cb02b213ea917c09ede4f4d0edd65d1caa0b
N00budrag0n <helmiagung2468@gmail.com>
2025-11-27 16:44:28 +0700

    model > add environment data table

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from .database import Base
 
-# class Temperature(Base):
-#     __tablename__ = "environment_data"
-#     id = Column(Integer, primary_key=True, index=True)
-#     machine_id = Column(String(100), index=True, nullable=True)
-#     temperature = Column(Float)
-#     humidity = Column(Float, nullable=True)
-#     timestamp = Column(DateTime, index=True)
+class Temperature(Base):
+    __tablename__ = "environment_data"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    machine_id = Column(String(100), index=True, nullable=True)
+    temperature = Column(Float)
+    humidity = Column(Float, nullable=True)
+    timestamp = Column(DateTime, index=True)
 
 class VendingSale(Base):
     __tablename__ = "vending_sales"
