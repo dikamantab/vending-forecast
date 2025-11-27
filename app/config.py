f515f2bd4bfc52232f865eb9c4e36e60ebca6eae
N00budrag0n <helmiagung2468@gmail.com>
2025-11-27 16:43:41 +0700

    config > update database url and ml model

--- app/config.py
+++ app/config.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str 
+    DATABASE_URL: str = "mysql+pymysql://root:@127.0.0.1:3306/vending_forecast"
     MQTT_BROKER_HOST: str 
     MQTT_BROKER_PORT: int 
     MQTT_USERNAME: str 
     MQTT_PASSWORD: str 
     MQTT_TOPIC: str 
-    ML_MODEL_PATH: str 
+    ML_MODEL_PATH: str = "app/ml/xgb_model.json" 
     DOWNLOAD_FOLDER: str 
 
     VENDOR_URL: str 
