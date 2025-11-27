3a80608d44b85bbe7fa9bbf88c653aacaec3fb80
N00budrag0n <helmiagung2468@gmail.com>
2025-11-27 13:58:47 +0700

    ml service > change loader from sklearn to xgboost

--- app/services/ml_service.py
+++ app/services/ml_service.py
@@ -1,11 +1,14 @@
-import joblib
+import xgboost as xgb
 import pandas as pd
 from app.config import settings
 
 class MLService:
-    def __init__(self, model_path=None):
+    def __init__(self, model_path="app/ml/xgb_model.json"):
+        # path = model_path or settings.ML_MODEL_PATH
+        # self.model = joblib.load(path)
         path = model_path or settings.ML_MODEL_PATH
-        self.model = joblib.load(path)
+        self.model = xgb.Booster()
+        self.model.load_model(path)
 
     def predict(self, df: pd.DataFrame):
         # If you have preprocessing pipeline saved as joblib, load it here and transform.
