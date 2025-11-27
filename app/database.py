9dccff597f7cfce15639343dc606aa54501566d5
N00budrag0n <helmiagung2468@gmail.com>
2025-11-27 16:44:05 +0700

    database > update url

--- app/database.py
+++ app/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
